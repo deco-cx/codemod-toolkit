2b81f7806b72357170d6f9009ccb0452c3358692
Marcos Candeia <marrcooos@gmail.com>
2024-09-06 09:27:14 -0300

    Migrate from deco-cx/deco
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

---
+++ src/pkg.ts
@@ -0,0 +1,36 @@
+import * as semver from "@std/semver";
+import { lookup, REGISTRIES, type RegistryUrl } from "./registry.ts";
+
+export interface PackageInfo {
+  url: RegistryUrl;
+  versions: {
+    current: string;
+    latest?: string;
+  };
+}
+
+function eligibleLatestVersion(versions: string[], allowPre = false) {
+  return allowPre
+    ? versions[0]
+    : versions.find((ver) => semver.parse(ver)?.prerelease?.length === 0);
+}
+
+export const pkgInfo = async (
+  importUrl: string,
+  allowPre = false,
+): Promise<PackageInfo | undefined> => {
+  const url = lookup(importUrl, REGISTRIES);
+
+  if (!url) return;
+
+  const versions = await url.all();
+  const current = url.version();
+  const latest = eligibleLatestVersion(versions, allowPre);
+  return {
+    url,
+    versions: {
+      current,
+      latest,
+    },
+  };
+};
